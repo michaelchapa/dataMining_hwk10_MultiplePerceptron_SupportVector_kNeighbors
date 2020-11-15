6b29e51d36ecff6a4df0fe97820bbd3f2c364bee
Michael Chapa <michaelchapa1@gmail.com>
2020-11-15 17:46:51 -0600

    Finished subRoutine.

--- hwk10.py
+++ hwk10.py
@@ -1,6 +1,7 @@
 import numpy as np
 import pandas as pd
 from sklearn.neural_network import MLPClassifier
+from sklearn.svm import SVC
 from sklearn.datasets import make_classification
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import MinMaxScaler
@@ -66,10 +67,59 @@ def create_NeuralNetwork_classify(X_train, X_test, y_train, y_test):
           "hidden layer size: 200, activation fxn: Logistic\n" +
           "Accuracy: %.2lf, Loss: %.2lf" % (clf2_accuracy, clf2_loss))
 
+###################### create_supportVector_classify #########################
+# Purpose:
+#   Creates multiple C-Support Vector classifiers with various parameters.
+#   We're observing the different Accuracies.
+# Parameters:
+#   X_train     Array       Training attributes, proportional to y_train
+#   X_test      Array       Testing attributes, proportional to y_test
+#   y_train     Array       Training class labels, proportional to X_train
+#   y_test      Array       Testing class labels, proportional to X_test
+# Returns:
+#   None
+# Notes:
+#   None
+def create_supportVector_classify(X_train, X_test, y_train, y_test):
+    clf_rbf = SVC(C = .1, gamma = "auto").fit(X_train, y_train)
+    clf_rbf.predict(X_test)
+    clf_rbf_accuracy = clf_rbf.score(X_test, y_test)
+    
+    clf_poly = SVC(C = .001, gamma = "auto", kernel = "poly").fit(X_train, y_train)
+    clf_poly.predict(X_test)
+    clf_poly_accuracy = clf_poly.score(X_test, y_test)
+    
+    clf_sig = SVC(C = 1, gamma = "auto", kernel = "sigmoid").fit(X_train, y_train)
+    clf_sig.predict(X_test)
+    clf_sig_accuracy = clf_sig.score(X_test, y_test)
+    
+    print("C-Support Vector Classifier, " + 
+          "C: .1, kernel: rbf\n" +
+          "Accuracy: %.2lf\n" % (clf_rbf_accuracy))
+    print("C-Support Vector Classifier, " + 
+          "C: .001, kernel: poly\n" +
+          "Accuracy: %.2lf\n" % (clf_poly_accuracy))
+    print("C-Support Vector Classifier, " + 
+          "C: 1, kernel: sigmoid\n" +
+          "Accuracy: %.2lf\n" % (clf_sig_accuracy))
+    
+#################### create_kNeighbors_classify ##############################
+# Purpose:
+#   None
+# Parameters:
+#   None
+# Returns:
+#   None
+# Notes:
+#   None
+def create_kNeighbors_classify(X_train, X_test, y_train, y_test):
+    print("Hola!")
+    
 
 def main():
     X_train, X_test, y_train, y_test = trainTestSplit_data("hwk10.csv")
-    create_NeuralNetwork_classify(X_train, X_test, y_train, y_test)
+    # create_NeuralNetwork_classify(X_train, X_test, y_train, y_test)
+    create_supportVector_classify(X_train, X_test, y_train, y_test)
     
 if __name__ == "__main__":
     main()
