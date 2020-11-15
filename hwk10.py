f113f7664fafbc9f4cb4307a8f9b1609f0dabaa6
Michael Chapa <michaelchapa1@gmail.com>
2020-11-15 16:21:44 -0600

    Dropped a useless subRoutine and finished the MLP subRoutine. beginning another classifier.

--- hwk10.py
+++ hwk10.py
@@ -4,26 +4,6 @@ from sklearn.neural_network import MLPClassifier
 from sklearn.datasets import make_classification
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import MinMaxScaler
-
-############################# generateData ###################################
-# Purpose:
-#   Creates 20 data tuples based on csv data, newData is scaled to original
-#   data dimensionality and values.
-# Parameters:
-#   csvFile    csv file    n-dimensional features and class label
-# Returns:
-#   newData    numpyArray  generated data with n-feature dimensionality. 
-#                          Doesn't return class label for each instance
-# Notes:
-#   Honestly, quite a useless function.
-def generateData(csvFile):
-    data = pd.read_csv(csvFile).to_numpy()
-    X, y = data[:, 0:-1], data[:, -1]
-    
-    scaler = MinMaxScaler()
-    X_new = scaler.fit_transform(X, y)
-    
-    return X_new[0:20, :]
     
 ########################## trainTestSplit_data ###############################
 # Purpose:
@@ -45,15 +25,15 @@ def trainTestSplit_data(csvFile):
         train_test_split(data[:, 0:-1], data[:, -1], test_size = 20)
         
     print("Split Training: X:", np.shape(X_train), "y:", np.shape(y_train))
-    print("Split Testing: X:", np.shape(X_test), "y:", np.shape(y_test))
+    print("Split Testing: X:", np.shape(X_test), "y:", np.shape(y_test), "\n")
     
     return X_train, X_test, y_train, y_test
 
 ###################### create_NeuralNetwork_classify #########################
 # Purpose:
-#   Creates a multiple level perceptron classifier to classify several unseen
+#   Creates a multiple level perceptron classifier to classify unseen
 #   data. We create multiple classifiers with various hidden layers and 
-#   activation functions. We check their accuracy.
+#   activation functions. We check their accuracy and loss.
 # Parameters:
 #   X_train     Array       Training attributes, proportional to y_train
 #   X_test      Array       Testing attributes, proportional to y_test
@@ -64,12 +44,32 @@ def trainTestSplit_data(csvFile):
 # Notes:
 #   None
 def create_NeuralNetwork_classify(X_train, X_test, y_train, y_test):
-    print("Hi :P")
+    # Train the classifiers
+    clf_100_relu = MLPClassifier(max_iter = 10000).fit(X_train, y_train)
+    clf_200_logistic = MLPClassifier(hidden_layer_sizes= 200, \
+                activation = 'logistic', max_iter = 10000).fit(X_train, y_train)
+        
+    # Print the mean accuracy/ loss on the given test data and labels
+    clf_100_relu.predict(X_test)
+    clf_200_logistic.predict(X_test)
+    
+    clf1_loss = round(clf_100_relu.loss_, 2)
+    clf1_accuracy = round(clf_100_relu.score(X_test, y_test), 2)
+    
+    clf2_loss = round(clf_200_logistic.loss_, 2)
+    clf2_accuracy = round(clf_200_logistic.score(X_test, y_test), 2)
+    
+    print("Multi-layer Perceptron Classifier, " + 
+          "hidden layer size: 100, activation fxn: RELU\n" +
+          "Accuracy: %.2lf, Loss: %.2lf\n" % (clf1_accuracy, clf1_loss))
+    print("Multi-layer Perceptron Classifier, " + 
+          "hidden layer size: 200, activation fxn: Logistic\n" +
+          "Accuracy: %.2lf, Loss: %.2lf" % (clf2_accuracy, clf2_loss))
 
 
 def main():
     X_train, X_test, y_train, y_test = trainTestSplit_data("hwk10.csv")
+    create_NeuralNetwork_classify(X_train, X_test, y_train, y_test)
     
-
 if __name__ == "__main__":
     main()
